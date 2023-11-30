302a8816703473cdee5c35732b5cd6d929a5b64e
Sarvesh Suryakant Arondekar <115332581+sarvesh11030801@users.noreply.github.com>
2023-11-30 15:10:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+gsap.to("#box", {
+    x:700,
+    duration:2,
+    delay:1,
+    backgroundColor:"pink",
+    scale:0.5,
+    rotate:360,
+    yoyo:true,
+    repeat:-1
+})
+
+// gsap.from("#box", {
+//     x:700,
+//     duration:2,
+//     delay:1,
+//     backgroundColor:"pink",
+//     scale:0.5,
+//     rotate:360,
+//     yoyo:true,
+//     repeat:-1
+// })
